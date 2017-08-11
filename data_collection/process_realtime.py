f876bbc3b4a9c9b2fbf6ea38601538734de50e64
潘星宇 <421915293@163.com>
2017-08-11 11:37:38 +0800

    init

---
+++ data_collection/process_realtime.py
@@ -0,0 +1,92 @@
+import serial
+import datetime
+
+import matplotlib.pyplot as pl
+import numpy as np
+import time
+
+a = serial.Serial('COM10',115200)
+WINDOW_SIZE = 1500
+SAMPLE_SIZE = 400
+
+fig = pl.figure("ACCE")
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlim([10,100])
+ax.set_ylim([0,120000])
+lines = []
+
+
+def plot(data):
+  x = np.array(range(len(data[0])//2+1))/len(data[0])*4000
+  x_freq = abs(np.fft.rfft(np.array(data[0], dtype=np.float32)))
+  y_freq = abs(np.fft.rfft(np.array(data[1], dtype=np.float32)))
+  z_freq = abs(np.fft.rfft(np.array(data[2], dtype=np.float32)))
+  freqs = [x_freq, y_freq, z_freq]
+  for i in range(3):
+    lines[i].set_xdata(x[1:])
+    lines[i].set_ydata(freqs[i][1:])
+
+  fig.canvas.draw()
+  fig.canvas.flush_events()
+
+
+
+  # # print(x_freq)
+  # pl.axis([10,300,0,120000])
+  # pl.plot(x[1:], x_freq[1:], 'r')
+  # # pl.draw()
+  # # pl.pause(0.001)
+
+  # pl.plot(x[1:], y_freq[1:], 'b')
+  # # pl.draw()
+  # # pl.pause(0.001)
+
+  # pl.plot(x[1:], z_freq[1:], 'k')
+  # pl.draw()
+  pl.pause(0.001)
+  # pl.clf()
+
+
+def run():
+  pl.ion()
+  pl.show()
+  a.write(b'\x01')
+  ret_data = [[0 for i in range(WINDOW_SIZE)], [0 for i in range(WINDOW_SIZE)], [0 for i in range(WINDOW_SIZE)]]
+  pl.show()
+  index = 0
+  sample_num = 0
+  for i in range(3):
+    line, = ax.plot(ret_data[i])
+    lines.append(line)
+  while True:
+    data = a.read(6)
+    # print(data)
+    num = []
+    for i in range(3):
+      num.append(int(data[i*2]<<8 | data[i*2+1]))
+      if num[i] > 2**15:
+        num[i] -= 2**16
+    for i in range(3):
+      ret_data[i][index] = num[i]
+    index += 1
+    sample_num += 1
+    if index == WINDOW_SIZE:
+      index = 0
+    if sample_num == SAMPLE_SIZE:
+      plot(ret_data)
+      sample_num = 0
+
+run()
+
+
+'''
+连线
+模块 -> 开发板
+VCC     3.3
+GND     G
+SCLK    A5
+SDI     A7
+SDO     A6
+INT     A1
+NCS     A4
+'''
---
+++ data_process/reader.py
@@ -0,0 +1,81 @@
+import argparse
+import csv
+import json
+
+def reader_csv(csv_file_name):
+    key_timestamps = []
+    with open(csv_file_name, 'r') as csv_file_handle:
+        spamreader = csv.reader(csv_file_handle)
+        title_flag = False
+        for row in spamreader:
+            if not title_flag:
+                title_flag = True
+                continue
+            start_time = int(row[2])
+            end_time = int(row[3])
+            key_timestamps.append((start_time, end_time))
+    # print(key_timestamps)
+    return key_timestamps
+
+
+def reader_acc(acc_file_name, key_timestamps, tense_tag):
+    acc_data_list = []
+    with open(acc_file_name, 'r') as acc_file_handle:
+        acc_raw_data_list = acc_file_handle.readlines()
+        acc_raw_data_length = len(acc_raw_data_list)
+        if tense_tag > 0:
+            key_No = 0
+            gestures_num = len(key_timestamps)
+            choose_flag = False
+            acc_data_per_gesture = [[], [], []]
+            for data_str in acc_raw_data_list:
+                acc_raw_data = [int(x) for x in data_str.split()]
+                if choose_flag and acc_raw_data[0] >= key_timestamps[key_No][1]:
+                    choose_flag = False
+                    key_No += 1
+                    acc_data_list.append([acc_data_per_gesture, tense_tag])
+                    acc_data_per_gesture = [[], [], []]
+                    if key_No >= gestures_num:
+                        break
+                    continue
+                if not choose_flag and acc_raw_data[0] >= key_timestamps[key_No][0]:
+                    choose_flag = True
+                if choose_flag:
+                    acc_data_per_gesture[0].append(acc_raw_data[1])
+                    acc_data_per_gesture[1].append(acc_raw_data[2])
+                    acc_data_per_gesture[2].append(acc_raw_data[3])
+        else:
+            acc_data_per_gesture = [[], [], []]
+            for data_str in acc_raw_data_list:
+                acc_raw_data = [int(x) for x in data_str.split()]
+                acc_data_per_gesture[0].append(acc_raw_data[1])
+                acc_data_per_gesture[1].append(acc_raw_data[2])
+                acc_data_per_gesture[2].append(acc_raw_data[3])
+            acc_data_list = [acc_data_per_gesture, tense_tag]
+
+    return acc_data_list
+
+
+def print_data(acc_data_list, output_dir):
+    with open(output_dir, 'w') as file_handle:
+        file_handle.write(json.dumps(acc_data_list))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('tag', type=int)
+    parser.add_argument('acc', type=str)
+    parser.add_argument('--csv', type=str)
+    parser.add_argument('--output_dir', type=str, default="./acc_data.json")
+    args = parser.parse_args()
+    csv_file_name = args.csv
+    acc_file_name = args.acc
+    tag = args.tag
+    output_dir = args.output_dir
+    if tag > 0:
+        key_timestamps = reader_csv(csv_file_name)
+    else:
+        key_timestamps = []
+    acc_data_list = reader_acc(acc_file_name, key_timestamps, tag)
+    print_data(acc_data_list, output_dir)
+
+main()
